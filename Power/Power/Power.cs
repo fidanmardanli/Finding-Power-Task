f21e778c522e2adc092c118fe13168c999d76c1d
fidanmardanli <fidannm@code.edu.az>
2022-07-02 19:56:39 +0400

    task about finding the power

---
+++ Power/Power/Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    public static class Power
+    {
+        public static int FindingPower(this int num, int num2)
+        {
+            int count = 1;
+            for (int i = 1; i <= num2; i++)
+                count = count * num;
+
+            return count;
+            
+        }
+    }
+} 
+       
---
+++ Power/Power/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Power
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 2;
+            Console.WriteLine(a.FindingPower(b));
+
+            //It will find the a^b. (10^2)
+
+
+        }
+
+    }
+}
